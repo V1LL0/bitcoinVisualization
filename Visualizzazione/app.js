07699b1d80c2c535f8b23064dfe65086526da8f0
massimo <v.cestarelli@gmail.com>
2014-06-25 15:03:00 +0200

    ciao

--- Visualizzazione/app.js
+++ Visualizzazione/app.js
@@ -48,38 +48,84 @@ http.createServer(app).listen(app.get('port'), function(){
 
 //router.get(...)
 app.get('/minerList', function(req, res) {
-//router.get(...)
-app.get('/minerList', function(req, res) {
   res.end(JSON.stringify(minerDictionary));
 });
 
+app.get('/minerInteractionsList', function(req, res) {
+  res.end(JSON.stringify(minersInteractions));
 });
 
-
-
 /////////MONGO////////////////
-
 var minerDictionary = {}
+var minersInteractions = {} // hash -> [hash_destinazioni]
+
+// var ObjectID = mongo.ObjectID;
+var BSON = mongo.BSONPure; 
 
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect("mongodb://localhost:27017/bitcoinDB", getMinerList);
+MongoClient.connect("mongodb://localhost:27017/bitcoinDB", initData);
 
 function getMinerList(err, db){
- var num = 0;
- db.collection('addresses').find({},{'_id':1}).toArray(function(err, items) {
-   var MAX = 50;
-   items.forEach(function(miner){
-     if (num<MAX){
-       minerDictionary[num] = miner['_id'];
-       num++;
-      }    
-    console.log(miner)
-     
-   });
+  var num = 0;
+  db.collection('addresses').find({'_type':'address'},{'_id':1}).toArray(function(err, items) {
+    var MAX = 50;
+    items.forEach(function(miner){
+      //if (num<MAX){
+        minerDictionary[num] = miner['_id'];
+        num++;
+      //}
+    });
+    // console.log(JSON.stringify(minerDictionary))
+    console.log("node loaded")    
+    getMinersInteraction(err, db);
+  });
+}
 
-   //console.log(JSON.stringify(minerDictionary))
-   //console.log("node loaded")    
-   //getMinersInteraction(err, db);
- });
+//Crea un dizionario in cui memorizza quali minatori hanno pagati altri minatori
+// function getMinersInteraction(err, db){ //TODO: da rendere asincrono: http://justinklemm.com/node-js-async-tutorial/
+//   Object.keys(minerDictionary).forEach(function(key){
+//     console.log(key)
+//     var miner_id = minerDictionary[key];
+//     console.log(miner_id)
+//     console.log(miner_id.length)    
+//     db.collection('addresses').findOne({_id: miner_id},{'tx_payment':1, '_id':0}, function(err, miner_txs){
+//       console.log(miner_txs);
+//       address_receving = miner_txs['addressesValue_receving'];
+//       if ( address_receving != null && address_receving.length > 0){
+//         console.log("miner_txs presenti")
+//         console.log(JSON.stringify(address_receving))
+//         minersInteractions[miner_id] = address_receving;
+//       }
+//     });
+//   });
+//   // console.log(JSON.stringify(minersInteractions))
+//   // console.log("links loaded")
+
+//   // console.log("all data are loaded")
+// }
+
+//VERSIONE DI PROVA
+function getMinersInteraction(err, db){
+  function random(start, end){
+    return parseInt(Math.random()*(end-start)) + start;
+  }
+  var num_links = random(10,30);
+  dictonary_length = Object.keys(minerDictionary).length;
+  for (var i=0; i<num_links; i++){
+    key = random(0, dictonary_length);
+    hash = minerDictionary[key];
+    links = [];
+    num_links_per_node = random(0,10);
+    for (var j=0; j<num_links_per_node; j++)
+      links.push( minerDictionary[random(0, dictonary_length)] );
+    minersInteractions[hash] = links;
+  }
+
+  console.log(JSON.stringify(minersInteractions));
+  console.log("links loaded");
+}
+
+function initData(err, db){
+  getMinerList(err, db);
 }
--- Visualizzazione/public/js/chord.js
+++ Visualizzazione/public/js/chord.js
@@ -2,11 +2,19 @@ function getJSONFromString(string){
 	return jQuery.parseJSON( string );
 }
 
+function callGet(path, callback){
+  $.ajax({
+    type: 'get',
+    url: path,
+    success: function(data){ callback(data); }
+  });
+}
+
+
 //Data must be a matrix of dependencies. The first item must be the main package.
 //For instance, if the main package depends on packages A and B, and package A
 //also depends on package B, you should build the data as follows:
-
-var data = {
+var data; /*{
 		packageNames: ['A', 'B', 'C', 'D', 'E', 'F'],
 		matrix: [[0, 1, 1, 1, 1, 1], // Main depends on A and B
 		         [0, 0, 1, 1, 0, 1], // A depends on B
@@ -15,24 +23,40 @@ var data = {
 		         [1, 1, 0, 0, 0, 0],
 		         [1, 1, 0, 1, 1, 0]] // B doesn't depend on A or Main
 };
+*/
+	var numHashM;
+	var hashto_HashList;
+
+
+function initializeData(createDataCallback, startCallback){
+	callGet('/minerList', function(data){
+									numHashM = data;
+									callGet('/minerInteractionsList', function(data){
+																		hashto_HashList = data
+																		createDataCallback(numHashM, hashto_HashList, startCallback);
+																	});
+									});
+	//{'1':'abc', '2':'bdfg', '3':'casfg', '4':'dasfg', '5':'ekiuj', '6':'fertey'};
+	//{'abc':['bdfg', 'casfg'], 'dasfg':['casfg', 'ekiuj', 'bdfg'], 'casfg':['fertey', 'abc', 'bdfg'], 
+	//'dasfg':['abc'], 'ekiuj':['abc', 'bdfg'], 'fertey':['abc', 'bdfg', 'ekiuj']};
+
+	
+
+}
 
 
 
-
-var numHashM = {'1':'abc', '2':'bdfg', '3':'casfg', '4':'dasfg', '5':'ekiuj', '6':'fertey'};
-var hashto_HashList = {'abc':['bdfg', 'casfg'], 'dasfg':['casfg', 'ekiuj', 'bdfg'], 'casfg':['fertey', 'abc', 'bdfg'], 
-		'dasfg':['abc'], 'ekiuj':['abc', 'bdfg'], 'fertey':['abc', 'bdfg', 'ekiuj']};
-
-
-
-
-function createData(numberHashMap, hashto_HashList){
+function createData(numberHashMap, hashto_HashList, startCallback){
 	data={
 			packageNames:[],
 			matrix: []
 	}
+	// console.log(numberHashMap);
+	// console.log(hashto_HashList);
 
-	
+	numberHashMap = getJSONFromString(numberHashMap);
+	hashto_HashList = getJSONFromString(hashto_HashList);
+
 	data.packageNames = Object.keys(numberHashMap);
 		
 	var row = [];
@@ -57,15 +81,17 @@ function createData(numberHashMap, hashto_HashList){
 		data.matrix.push(row);			
 
 	});
-	return data;
+
+	start();
+
 }
 
 
 
-data = createData(numHashM, hashto_HashList);
+initializeData(createData, start);
 
 
-
+function start(){
 //Dependency wheel chart for d3.js
 
 //Usage:
@@ -82,3 +108,4 @@ data = createData(numHashM, hashto_HashList);
 	//You can customize the chart width, margin (used to display package names),
 	//and padding (separating groups in the wheel)
 	//var chart = d3.chart.dependencyWheel().width(700).margin(150).padding(.02);
+}
